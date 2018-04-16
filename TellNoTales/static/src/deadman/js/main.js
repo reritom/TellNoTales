7ab346d5bc71317e82863504436fab2a0063f03b
reritom <reikudjinn@gmail.com>
2018-04-16 22:47:40 +0200

    Basic components added for single-contact, single-message, message-group

--- TellNoTales/static/src/deadman/js/main.js
+++ TellNoTales/static/src/deadman/js/main.js
@@ -1,7 +1,21 @@
-Vue.component('blog-post', {
-  props: ['type'],
-  template: '<h3>Helloworld {{type}}</h3>'
+Vue.component('single-message', {
+  props: ['messagedata'],
+  template: '<div><h3>This is a single message with subject {{messagedata.subject}}</h3><p>And message {{messagedata.message}}</p></div>'
 })
+
+Vue.component('message-group', {
+  props: ['messagelist'],
+  template: '<div><div v-for="amessage in messagelist"><single-message :messagedata="amessage"></single-message></div></div>'
+})
+
+Vue.component('single-contact', {
+  props: ['contactdata'],
+  template: '<div><h3>This is a single contact with name {{contactdata.name}}</h3><p>And email addresses {{contactdata.email_addresses}}</p></div>'
+})
+
+// Create a contact-group component
+// New message component
+// New contact component
 
 
 new Vue({
@@ -38,6 +52,21 @@ getMessages: function() {
           console.log(response.data);
             console.log(response.data.data);
       this.messages = response.data.data.messages;
+      this.loading = false;
+      })
+      .catch((err) => {
+       this.loading = false;
+       console.log(err);
+      })
+},
+getContacts: function() {
+  this.loading = true;
+  this.$http.get('/api/contact/')
+      .then((response) => {
+        console.log(response);
+          console.log(response.data);
+            console.log(response.data.data);
+      this.contacts = response.data.data.contacts;
       this.loading = false;
       })
       .catch((err) => {
